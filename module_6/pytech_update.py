d99c4caca2af8a82bdd18247bad2cef2977b3bc9
James jpb <jpbefx@hotmail.com>
2022-06-23 22:43:49 -0500

    Finished changes to module6.2
    final changes to module6.2. Moving to module 6.3.

--- module_6/pytech_update.py
+++ module_6/pytech_update.py
@@ -16,6 +16,25 @@ db = client.pytech
 # Switch to collection
 collection = db["students"]
 
+# Print a header for information as to what is being displayed
+print("-- DISPLAYING STUDENTS DOCUMENT FROM find() QUERY --")
+
 # Find all of the document contents
 for module6_2 in collection.find({},{'_id':0}):
     print(module6_2)
+
+# call the update_one method by student_id 1007 and 
+# update the last name to something different than the originaly saved name.
+m6_2Update = collection.update_one({"Student_ID": "1007"}, {"$set": {"Last Name": "Bond"}})
+
+print()
+print()
+
+# Print a header for information as to what is being displayed
+print("-- DISPLAYING UPDATED STUDENT DOCUMENT FROM find_one() QUERY --")
+
+# Display updated student document from find_one query
+alterEgo = collection.find_one({"Student_ID": "1007"}, {"_id":0})
+
+print(alterEgo)
+
